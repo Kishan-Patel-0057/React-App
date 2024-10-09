efb4252aa91195e27295d48a6b29e10c5077a39c
Kishan Patel <kishan@addact.net>
2024-10-09 12:42:07 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
     const navigate = useNavigate();
   
     const handleClick = () => {
-      navigate('/blog');
+      navigate('https://addact-dev.netlify.app/blog');
     };
   return (
     <div className="home">
